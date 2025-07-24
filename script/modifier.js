dccaaf6bdb38d9e20f4880ef814edb88078620c0
Audrey <choussiii69@gmail.com>
2025-07-24 15:25:57 +0200

    les pages javascript pour toutes mes pages

---
+++ script/modifier.js
@@ -0,0 +1,15 @@
+function previewPhoto(event) {
+            const input = event.target;
+            const preview = document.getElementById('photoPreview');
+            
+            if (input.files && input.files[0]) {
+                const reader = new FileReader();
+                
+                reader.onload = function(e) {
+                    preview.style.display = 'block';
+                    preview.src = e.target.result;
+                }
+                
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
